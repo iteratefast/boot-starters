829dff2284c03a07aaca4ab82d4608ec28335d5a
liuhao <liuhao@rongcapital.cn>
2018-06-11 16:28:54 +0800

    取消bean改为配置

--- restful-spring-boot-starter/src/main/java/iteratefast/top/bootstarter/restful/RestfulStarterConfiguration.java
+++ restful-spring-boot-starter/src/main/java/iteratefast/top/bootstarter/restful/RestfulStarterConfiguration.java
@@ -1,11 +1,8 @@
 package iteratefast.top.bootstarter.restful;
 
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.DispatcherServlet;
 
 import iteratefast.top.bootstarter.restful.validator.handler.ControllerAdviceExceptionHandler;
 
@@ -13,11 +10,4 @@ import iteratefast.top.bootstarter.restful.validator.handler.ControllerAdviceExc
 @Import({ ControllerAdviceExceptionHandler.class })
 @PropertySource("classpath:starter.properties")
 public class RestfulStarterConfiguration {
-	@Bean
-	public ServletRegistrationBean<DispatcherServlet> dispatcherRegistration(DispatcherServlet dispatcherServlet) {
-		dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
-		ServletRegistrationBean<DispatcherServlet> reg = new ServletRegistrationBean<DispatcherServlet>(
-				dispatcherServlet);
-		return reg;
-	}
 }
